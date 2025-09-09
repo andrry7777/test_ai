0a48cc52f112d8716a6218cbdffb5ddea2180769
Andy <153993325+andrry7777@users.noreply.github.com>
2025-09-09 16:55:01 +0900

    Use Open-Meteo API for weather

--- weather_forecast_app/lib/features/weather/infrastructure/weather_repository_impl.dart
+++ weather_forecast_app/lib/features/weather/infrastructure/weather_repository_impl.dart
@@ -1,11 +1,31 @@
+import 'package:dio/dio.dart';
+import 'package:weather_forecast_app/shared/exceptions.dart';
+
 import '../domain/entities/current_weather.dart';
 import '../domain/entities/location.dart';
 import '../domain/repositories/weather_repository.dart';
 
 class WeatherRepositoryImpl implements WeatherRepository {
+  WeatherRepositoryImpl({Dio? dio}) : _dio = dio ?? Dio();
+
+  final Dio _dio;
+
   @override
   Future<CurrentWeather> getCurrentWeather(Location location) async {
-    // TODO: implement actual API call
-    return const CurrentWeather(temperature: 0);
+    final response = await _dio.get(
+      'https://api.open-meteo.com/v1/forecast',
+      queryParameters: {
+        'latitude': location.lat,
+        'longitude': location.lon,
+        'current_weather': true,
+      },
+    );
+    final data = response.data;
+    final current = data['current_weather'];
+    if (current == null) {
+      throw NetworkException('No current weather data');
+    }
+    final temperature = (current['temperature'] as num).toDouble();
+    return CurrentWeather(temperature: temperature);
   }
 }
